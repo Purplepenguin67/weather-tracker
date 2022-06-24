ee7f7ff3c52680a632afa968d8bb809582b8452c
Douglas Snodgrass <douglassnodgrass@Mac-mini.lan>
2022-06-23 21:53:13 -0700

    wind speed

--- script.js
+++ script.js
@@ -7,3 +7,45 @@ var speed = $("windspeed");
 var city = $("city");
 var clear = $("clear");
 var temp = $("temp");
+
+
+
+function currentcityWeather(city){
+    
+    var queryURL= "https://api.openweathermap.org/data/2.5/weather?q=" + city + "&APPID=" + APIKey;
+    $.ajax({
+        url:queryURL,
+        method:"GET",
+    }).then(function(response){
+
+        console.log(response);
+        
+        var wicon= response.weather[0].icon;
+        var iconurl="https://openweathermap.org/img/wn/"+wicon +"@2x.png";
+
+        var date=new Date(response.dt*1000).toLocaleDateString();
+        
+        $(currentCity).html(response.name +"("+date+")" + "<img src="+iconurl+">");
+
+        var date=new Date(response.dt*1000).toLocaleDateString();
+        
+        $(currentCity).html(response.name +"("+date+")" + "<img src="+iconurl+">
+
+        var fahrenheit = (response.main.temp- 273.15) * 1.80 + 32;
+        $(temp).html((fahrenheit).toFixed(2)+"&#8457");
+        $(humidity).html(response.main.humidity+"%");
+        var ws=response.wind.speed;
+        var windsmph=(ws*2.237).toFixed(1);
+        $(speed).html(windsmph+"MPH");
+        uvindex(response.coord.lon,response.coord.lat);
+        forcast(response.id);
+        if(response.cod==200){
+
+
+            
+        }
+
+
+
+
+    
